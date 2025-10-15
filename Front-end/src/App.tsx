b32f5934ef034a593a3481e88d7b122ce3035135
lucasfabs <lucasfabsuniv@gmail.com>
2025-10-15 10:34:05 +0200

    first commit front

---
+++ Front-end/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-amber-700 font-bold underline">
+        twittor
+      </h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
