6d201a541068a07e1e7a7bb712cfd0c377299076
lucasfabs <lucasfabsuniv@gmail.com>
2025-10-15 11:09:01 +0200

    signup commit

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -1,14 +1,9 @@
-import { Button } from "@/components/ui/button"
-
-
+import { Signup1 } from "./pages/signup"
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl text-amber-700 font-bold underline">
-        twittor
-      </h1>
-      <Button>Click me</Button>
+      <Signup1 />
     </>
   )
 }
